7680872e012f9ba662ab82982ef4434da063fa04
danglenzig <brad.neal@student.forsbergsskola.se>
2026-01-28 20:10:37 +0100

    further testing in the demo scene

--- Assets/DataService/Demo/DemoSceneController.cs
+++ Assets/DataService/Demo/DemoSceneController.cs
@@ -1,193 +1,144 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
 using System.Linq;
 using BradsDataService;
 
 
-public enum EnumQuestStatus
+public static class StatusEffects
 {
-    NOT_STARTED,
-    STARTED,
-    FINISHED,
+    public const string HUNGRY = "HUNGRY";
+    public const string ANGRY = "ANGRY";
+    public const string HORNY = "HORNY";
+    public const string HORNGRY = "HORNGRY";
+    public static List<string> FX = new List<string> { HUNGRY, ANGRY, HUNGRY, HORNGRY };
+}
+
+public class Quests
+{
+    public const string QUEST_1 = "Do a little dance";
+    public const string QUEST_2 = "Make a little love";
+    public const string QUEST_3 = "Get down tonight";
 }
 
 public class DemoSceneController : MonoBehaviour
 {
-    [SerializeField] private FieldSO playerNameField;           // string
-    [SerializeField] private FieldSO playerStatusEffectField;   // List<string>
-    [SerializeField] private FieldSO playerPositionField;       // Vector3
-    [SerializeField] private FieldSO playerHealthField;         // int
-    [SerializeField] private FieldSO secondsPlayedField;        // float
-    [SerializeField] private FieldSO playerMapPosition;         // Vector2
-    [SerializeField] private FieldSO playerColor;               // Vector4
-    [SerializeField] private FieldSO questStatuses;             // Dictionary<string, string>
-    [SerializeField] private FieldSO playerOnboarded;           // bool
+
+    [SerializeField] private DemoGameData gameData;
+    [SerializeField] private Button testButton;
+
+    private List<string> statusFX = new();
+    private Dictionary<string, EnumQuestStatus> questStatusDict = new();
+
+
+    private void OnEnable()
+    {
+        testButton.onClick.AddListener(OnTestButtonPressed);
+    }
+    private void OnDisable()
+    {
+        testButton.onClick.RemoveAllListeners();
+    }
 
     private void Start()
     {
-        SetTestPlayerName("Namey Nameson");
-        SetTestPlayerStatusFX();
-        SetTestPlayerPosition();
-        SetTestPlayerMapPos();
-        SetTestPlayerHealth();
-        SetTestSecondsPlayed();
-        SetTestPlayerColor();
-        SetTestPlayerOnboarded();
-        SetTestQuestStatuses();
-        DebugRuntimeData();
+        InitializeGameData();
+        DebugGameData();
     }
 
-    private void SetTestPlayerName(string pName)
+    private void OnTestButtonPressed()
     {
-        RuntimeDataService.TryUpdateRuntimeDatum<string>(pName, playerNameField.FieldID);
-    }
-    private void SetTestPlayerColor()
-    {
-        Vector4 colVector = new Vector4(.2f, .3f, .4f, .5f);
-        RuntimeDataService.TryUpdateRuntimeDatum<Vector4>(colVector, playerColor.FieldID);
+        RandomizeGameData();
+        DebugGameData();
     }
 
-    private void SetTestPlayerStatusFX()
+    private void InitializeGameData()
     {
-        List<string> fxList = new();
-        fxList.Add("HUNGRY");
-        fxList.Add("ANGRY");
-        fxList.Add("TIRED");
-        RuntimeDataService.TryUpdateRuntimeDatum<List<string>>(fxList, playerStatusEffectField.FieldID);
+        gameData.PlayerName = "Namey Nameson";
+        gameData.PlayerPos = new Vector3(1f, 2f, 3f);
+        gameData.PlayerHealth = 100;
+        gameData.SecondsPlayed = 0;
+        gameData.PlayerMapPos = new Vector2(100f, 200f);
+        gameData.PlayerColor = Color.aliceBlue;
+        gameData.PlayerOnboarded = false;
+
+        gameData.CurrentStatusEffects = statusFX;
+
+        questStatusDict[Quests.QUEST_1] = EnumQuestStatus.NOT_STARTED;
+        questStatusDict[Quests.QUEST_2] = EnumQuestStatus.NOT_STARTED;
+        questStatusDict[Quests.QUEST_3] = EnumQuestStatus.NOT_STARTED;
+        gameData.CurrentQuestStatuses = questStatusDict;
     }
 
-    private void SetTestPlayerPosition()
-    {
-        Vector3 pos = new Vector3(1f, 2f, 3f);
-        RuntimeDataService.TryUpdateRuntimeDatum<Vector3>(pos, playerPositionField.FieldID);
-    }
-    private void SetTestPlayerMapPos()
-    {
-        Vector2 mapPos = new Vector2(100f, 200f);
-        RuntimeDataService.TryUpdateRuntimeDatum<Vector2>(mapPos, playerMapPosition.FieldID);
-    }
-    private void SetTestPlayerHealth()
-    {
-        RuntimeDataService.TryUpdateRuntimeDatum<int>(100, playerHealthField.FieldID);
-    }
-    private void SetTestSecondsPlayed()
-    {
-        RuntimeDataService.TryUpdateRuntimeDatum<float>(10f, secondsPlayedField.FieldID);
-    }
-    private void SetTestPlayerOnboarded()
-    {
-        RuntimeDataService.TryUpdateRuntimeDatum<bool>(true, playerOnboarded.FieldID);
-    }
 
-    private void SetTestQuestStatuses()
+    private void RandomizeGameData()
     {
-        Dictionary<string, EnumQuestStatus> questDict = new();
-        questDict["Do a little dance"] = EnumQuestStatus.FINISHED;
-        questDict["Make a little love"] = EnumQuestStatus.STARTED;
-        questDict["Get down tonight"] = EnumQuestStatus.NOT_STARTED;
-        RuntimeDataService.TryUpdateRuntimeDatum<Dictionary<string, string>>(StringDictifyQuestDict(questDict), questStatuses.FieldID);
-    }
-    private Dictionary<string, string> StringDictifyQuestDict(Dictionary<string, EnumQuestStatus> _questDict)
-    {
-        Dictionary<string, string> questStringDict = new();
-        foreach(string questName in _questDict.Keys.ToList<string>())
+        // for demo purposes
+        gameData.PlayerName = System.Guid.NewGuid().ToString(); // whatever
+        
+        float randoFloat = UnityEngine.Random.Range(0f, 1f);
+        gameData.PlayerPos = new Vector3(randoFloat, randoFloat, randoFloat) * 10f;
+        gameData.PlayerMapPos = new Vector2(randoFloat, randoFloat) * 100f;
+
+        gameData.PlayerColor = new Color(randoFloat, randoFloat, randoFloat, randoFloat);
+
+        int randoInt = UnityEngine.Random.Range(1, 100);
+        gameData.PlayerHealth = randoInt;
+        gameData.PlayerOnboarded = randoInt % 2 == 0;
+        int randoFxIdx_1 = randoInt % StatusEffects.FX.Count;
+        int randoFXIdx_2 = (randoFxIdx_1 + 1) % StatusEffects.FX.Count;
+        gameData.CurrentStatusEffects = new List<string> { StatusEffects.FX[randoFxIdx_1], StatusEffects.FX[randoFXIdx_2] };
+        gameData.SecondsPlayed = Time.time;
+
+        int questStatusIdx = randoInt % 3;
+        EnumQuestStatus randoStatus = (EnumQuestStatus)Enum.GetValues(typeof(EnumQuestStatus)).GetValue(questStatusIdx);
+
+        Dictionary<string, EnumQuestStatus> newStatusDict = new Dictionary<string, EnumQuestStatus>(gameData.CurrentQuestStatuses);
+        foreach(string keyStr in newStatusDict.Keys.ToList<string>())
         {
-            questStringDict[questName] = _questDict[questName].ToString();
+            newStatusDict[keyStr] = randoStatus;
         }
-        return questStringDict;
+        gameData.CurrentQuestStatuses = newStatusDict;
+
+
+
     }
-    private bool TryGetQuestDictFromQuestStringDict(Dictionary<string, string> stringDict, out Dictionary<string, EnumQuestStatus> questDict)
+
+    private void DebugGameData()
     {
-        questDict = new();
-
-        foreach(string keyStr in stringDict.Keys.ToList<string>())
-        {
-            string valStr = stringDict[keyStr];
-            if (!System.Enum.TryParse<EnumQuestStatus>(valStr, out EnumQuestStatus status)) return false;
-            questDict[keyStr] = status;
-        }
-        return true;
+        string debugStr = string.Empty;
+        debugStr += $"Player name: {gameData.PlayerName}\n";
+        debugStr += $"Player pos: {gameData.PlayerPos}\n";
+        debugStr += $"Player map pos: {gameData.PlayerMapPos}\n";
+        debugStr += $"Player health: {gameData.PlayerHealth}\n";
+        debugStr += $"Seconds played: {gameData.SecondsPlayed}\n";
+        debugStr += $"Player color: {gameData.PlayerColor}\n";
+        debugStr += $"Player onboarded: {gameData.PlayerOnboarded}\n";
+        debugStr += $"{StringifyFXList()}\n";
+        debugStr += $"{StringifyQuestDict()}\n";
+        Debug.Log(debugStr);
     }
 
-    private void DebugRuntimeData()
-    {
-        if (RuntimeDataService.TryGetRuntimeData(out RuntimeData data))
-        {
-            IReadOnlyDictionary<string, FieldNameAndType> nameTypeDict = data.FieldNamesAndTypesByID;
-            IReadOnlyDictionary<string, IRuntimeDatum> valuesDict = data.Data;
-
-            string debugStr = string.Empty;
-
-            foreach (string id in data.FieldNamesAndTypesByID.Keys.ToList<string>())
-            {
-                string fieldName = nameTypeDict[id].fieldName;
-                EnumFieldType fieldType = nameTypeDict[id].fieldType;
-                IRuntimeDatum datum = valuesDict[id];
-
-                string valStr = string.Empty;
-
-                switch (fieldType)
-                {
-                    case EnumFieldType.STRING:
-                        valStr = (datum as RuntimeDatum<string>).Value; break;
-                    case EnumFieldType.INT:
-                        valStr = ((datum as RuntimeDatum<int>).Value).ToString(); break;
-                    case EnumFieldType.FLOAT:
-                        valStr = ((datum as RuntimeDatum<float>).Value).ToString(); break;
-                    case EnumFieldType.BOOL:
-                        valStr = ((datum as RuntimeDatum<bool>).Value).ToString(); break;
-                    case EnumFieldType.STRING_LIST:
-                        valStr = StringifyList((datum as RuntimeDatum<List<string>>).Value); break;
-                    case EnumFieldType.VECTOR_3:
-                        valStr = StrigifyVector3((datum as RuntimeDatum<Vector3>).Value); break;
-                    case EnumFieldType.VECTOR_2:
-                        valStr = StrigifyVector2((datum as RuntimeDatum<Vector2>).Value); break;
-                    case EnumFieldType.VECTOR_4:
-                        valStr = StringifyVector4((datum as RuntimeDatum<Vector4>).Value); break;
-                    case EnumFieldType.STRING_DICT:
-                        valStr = StringifyStringDict((datum as RuntimeDatum<Dictionary<string, string>>).Value); break;
-
-                }
-                string datumStr = $"{fieldName} -- {fieldType.ToString()}: {valStr}\n";
-                debugStr += datumStr;
-            }
-            Debug.Log(debugStr);
-        }
-    }
-
-    private string StringifyList(List<string> strList)
+    private string StringifyFXList()
     {
         string outStr = string.Empty;
-        foreach(string item in strList)
+        foreach(string fx in gameData.CurrentStatusEffects)
         {
-            outStr += $"{item}, ";
+            outStr += $"{fx}, ";
         }
         return outStr;
     }
 
-    private string StringifyStringDict(Dictionary<string, string> stringDict)
+    private string StringifyQuestDict()
     {
         string outStr = string.Empty;
-        foreach(string keyStr in stringDict.Keys.ToList<string>())
+        foreach(var kvp in gameData.CurrentQuestStatuses)
         {
-            outStr += $"{keyStr}: {stringDict[keyStr]}, ";
+            string keyStr = kvp.Key;
+            string valStr = kvp.Value.ToString();
+            outStr += $"{keyStr}: {valStr}, ";
         }
-        return outStr;
-    }
-
-    private string StrigifyVector2(Vector2 vector2)
-    {
-        string outStr = $"({vector2.x}, {vector2.y})";
-        return outStr;
-    }
-    private string StrigifyVector3(Vector3 vector3)
-    {
-        string outStr = $"({vector3.x}, {vector3.y}, {vector3.z})";
-        return outStr;
-    }
-    private string StringifyVector4(Vector4 vector4)
-    {
-        string outStr = $"({vector4.w}, {vector4.x}, {vector4.y}, {vector4.z})";
         return outStr;
     }
 }
